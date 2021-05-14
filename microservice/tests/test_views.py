256496ee9f25c363380522046d81cba98dca247e
Olalekan Adebari <adebari.olalekan.oluwaseun@gmail.com>
2021-05-14 03:07:13 +0100

    Clean up

--- microservice/tests/test_views.py
+++ microservice/tests/test_views.py
@@ -11,18 +11,17 @@ from microservice.models import Clients, Transactions
 class TestTransactionCreateView(APITestCase):
     def setUp(self) -> None:
         self.cliente = Clients.objects.create(webhook_url="http://www.google.com", api_key="key", name="client_1")
-    def test_create_transaction(self):
 
+    def test_create_transaction(self):
         data = {
             "transaction_reference": str(uuid.uuid4()),
-             "price": 234.10,
-             "status": "successful"
+            "price": 234.10,
+            "status": "successful"
         }
         opts = {
-            "data": json.dumps(data), "content_type":"application/json", "x-api-key": self.cliente.api_key
+            "data": json.dumps(data), "content_type": "application/json", "x-api-key": self.cliente.api_key
         }
         resp = self.client.post(reverse("create_transactions"), **opts)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
         tx = Transactions.objects.filter(transaction_reference=data["transaction_reference"])
         self.assertEqual(len(tx), 1)
 
